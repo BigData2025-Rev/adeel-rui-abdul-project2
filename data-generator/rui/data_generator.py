6f1578756564839aa6169ee068a7764cd3850835
rl4git <cucucucu4pastime@gmail.com>
2025-01-14 12:04:33 -0800

    Rui Li - added basic products information and data generator structure.

---
+++ data-generator/rui/data_generator.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+import os
+
+def data_generator():
+  products = pd.read_csv('/content/products.csv')
+  categories = pd.read_csv('/content/categories.csv')
+
+  """
+  define trendline
+  先是日期，圣诞节，春节，黑色星期五，几率更高
+  然后是地区，美国，中国最高，然后日本，韩国，欧洲国家等
+  然后是城市，随机吧，也可以弄些权重。
+  然后是产品，传入参数：日期，地区。
+  - 产品本身就有权重，PC，调味料等产品的权重高
+  - 然后，根据传入的地区参数
+    - 如果是中国，调味料权重拉高
+    - 如果是美国，PC权重拉高
+    - etc
+  - 然后，根据传入的时间参数
+    - 如果是冬季，棉衣，取暖物品拉高
+    - 如果是夏季，T恤，冰淇淋，空调，几率拉高
+    - 如果是黑色星期五，家电，电脑，几率拉高
+    - 如果是圣诞节，春节，食品几率拉高
+  """ 
+  
